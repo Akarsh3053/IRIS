1791876fa366584f2bb279c73f3b803dca1e972a
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-12-10 19:03:36 +0530

    sampled backgroud canvas

--- IRIS.py
+++ IRIS.py
@@ -1,16 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 # window
 window = tk.Tk()
-window.geometry("600x400")
 window.title("IRIS")
 
+bg_image = Image.open("bg.png")
+bg_image = bg_image.resize()
+background = ImageTk.PhotoImage(bg_image)
 
-'''
-Everything Goes Here ..................
-'''
+canvas = tk.Canvas(window, width=background.width(), height=background.height())
+canvas.pack()
 
+canvas.create_image(0, 0, image=background, anchor='nw')
 
 # run
 window.mainloop()
