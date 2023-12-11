b340efedb607fd71689d8f253e5d006839393bc8
Akarsh Bajpai <onlyakarsh@gmail.com>
2023-12-11 16:40:21 +0530

    updated win config

--- IRIS.py
+++ IRIS.py
@@ -2,12 +2,16 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 
-# window
+# window config
 window = tk.Tk()
+window.iconbitmap("assets/IRIS.ico")
 window.title("IRIS")
+x, y = window.winfo_screenwidth(), window.winfo_screenheight()
+window.geometry(f'{x}x{y}')
 
-bg_image = Image.open("bg.png")
-bg_image = bg_image.resize()
+# BG image config
+bg_image = Image.open("assets/bg.jpg")
+# bg_image = bg_image.resize()
 background = ImageTk.PhotoImage(bg_image)
 
 canvas = tk.Canvas(window, width=background.width(), height=background.height())
